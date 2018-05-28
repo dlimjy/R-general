48f64d00bea013e77ee61970cd2666a665bc97fb
Daniel <daniel.lim@quantium.com.au>
2018-05-28 17:32:58 +1000

    EDA, update munging and packages

--- Recipes/data_munging.R
+++ Recipes/data_munging.R
@@ -8,12 +8,22 @@ train <- fread("train.csv")
 
 train %>% select(Age) %>% is.na %>% table # Check distribution of nulls
 
-# Suppose we want to impute missing ages using the median, stratifying by gender
+# Imputing nulls, stratified
 age_med_M <- (train %>% filter(Sex == "male"))$Age %>% median(na.rm = TRUE) # filter first, select column with $, then apply a function
 age_med_F <- (train %>% filter(Sex == "female"))$Age %>% median(na.rm = TRUE)
 
-train$Age[is.na(train$Age)] <- -100
-train$Age[train$Sex == "male" & train$Age == -100] <- age_med_M
-train$Age[train$Sex == "female" & train$Age == -100] <- age_med_F
+train$Age[is.na(train$Age)] <- -100 # Set null to some arbitrary value
+train$Age <- ifelse(train$Age == -100, ifelse(train$Sex == "male", median_age_M, train$Age), train$Age) # Use that arbitrary value + straifying variable to replace
+train$Age <- ifelse(train$Age == -100, ifelse(train$Sex == "female", median_age_F, train$Age), train$Age)
+
+# Replacing blank strings
+train$TextColumn <- ifelse(train$TextColumn == "", "Replacement", train$TextColumn)
 
 
+# # Train test split ------------------------------------------------------
+
+split_ratio <- 0.8 # Specify split ratio
+split_size <- floor(0.8 * nrow(train)) # Floor of number of rows based on split ratio
+split_ind <- base::sample(seq_len(nrow(train)), size = split_size) # Generate index for splitting
+trainset <- train[split_ind,] 
+testset <- train[-split_ind,]
--- packages-setup.R
+++ packages-setup.R
@@ -1,5 +1,5 @@
 # Setup workspace
-pkgs <- c("tidyverse", "h2o", "gbm", "rpart", "data.table", "ROCR", "lubridate")
-# install.packages(pkgs)
+pkgs <- c("tidyverse", "h2o", "gbm", "rpart", "data.table", "ROCR", "lubridate", "randomForest")
 
+# install.packages(pkgs)
 lapply(pkgs, require, character.only = TRUE)
